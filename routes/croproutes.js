293ec29bf8cdadc1405b5a365248d8a21ce609c1
voidtech1002-cyber <voidtech1002@gmail.com>
2025-08-24 13:13:44 +0530

    Update croproutes.js

--- routes/croproutes.js
+++ routes/croproutes.js
@@ -6,6 +6,6 @@ const upload = require("../middleware/multer");
 router.post("/", upload.array("files", 5), cropController.createCrop);
 router.get("/", cropController.getAllCrops);
 router.get("/:userId", cropController.getAllCropsByUser);
-
+router.put("/:userId", cropController.updateCropByUserId);
 
 module.exports = router;
